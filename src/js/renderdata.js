0500712f455714941ee9e6e6ba93da34e9692a0d
omar <omarfarah2002@hotmail.com>
2025-04-28 15:14:10 +0200

    triyng to add title and downloaded pictures to each cards

--- src/js/renderdata.js
+++ src/js/renderdata.js
@@ -1,9 +1,27 @@
 export function renderData(data, type) {
     const contentArea = document.getElementById('app');
     contentArea.innerHTML = '';
+
+    // card
     data.slice(0, 6).forEach(item => {
         const card = document.createElement('div');
         card.classList.add('card');
+
+    //    title 
+        const title = document.createElement('h3');
+      title.textContent = item.name || item.title;
+      card.appendChild(title);
+
+    //   image
+      const img = document.createElement('img');
+        img.src = item.image;
+        img.alt = item.name || item.title || 'Star Wars';
+        img.classList.add('card-image');
+        card.appendChild(img);
+
+
         contentArea.appendChild(card);
       });
+
+    
   }
