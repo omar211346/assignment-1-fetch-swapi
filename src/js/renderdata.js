ec48af3d8e221fcb8e6ece5f0d68213f56ecf729
omar <omarfarah2002@hotmail.com>
2025-04-28 15:05:28 +0200

    triyng to create the card for each item

--- src/js/renderdata.js
+++ src/js/renderdata.js
@@ -1,4 +1,9 @@
 export function renderData(data, type) {
     const contentArea = document.getElementById('app');
     contentArea.innerHTML = '';
+    data.slice(0, 6).forEach(item => {
+        const card = document.createElement('div');
+        card.classList.add('card');
+        contentArea.appendChild(card);
+      });
   }
