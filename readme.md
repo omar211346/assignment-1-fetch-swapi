3e6f0407d0a95aec14c2a9cc36a50fb7c7d02223
omar <omarfarah2002@hotmail.com>
2025-06-05 06:09:56 +0200

    had to move the image folder to public cause in netlify it was crashing, and in npm run dev it was working fine

---
+++ readme.md
@@ -0,0 +1,20 @@
+
+
+this is  a simple fetch swapi. where the task is to fetch information from the swapi-api
+
+the star wars website is in different categories, it is about explaining the star wars world through pitcures and text. 
+
+## Technologies Used
+
+- React (functional components + hooks)
+- Vite
+- LocalStorage
+- HTML/CSS (custom responsive styling)
+- Git & GitHub
+
+## Deployment
+
+This app is deployed on netlify and tracked in github. 
+
+github link
+
--- src/js/imageMappe.js
+++ src/js/imageMappe.js
@@ -1,13 +1,13 @@
 export function getImage(category, index) {
-    const nameMap = {
-      films: "film",
-      people: "people",
-      planets: "planet",
-      vehicles: "vehicle"
-    };
-  
-    const prefix = nameMap[category] || category;
-  
-    return `src/img/${category}/${prefix}${index + 1}.jpg`;
-  }
-  
+  const nameMap = {
+    films: "film",
+    people: "people",
+    planets: "planet",
+    vehicles: "vehicle"
+  };
+
+  const prefix = nameMap[category] || category;
+
+
+  return `/img/${category}/${prefix}${index + 1}.jpg`;
+}
